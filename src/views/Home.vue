83f04665579210c928099d55e25985a7e6df7a49
Marcio Alves <MarcioApocallypse@gmail.com>
2022-07-07 23:44:56 -0300

    finish projeto

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <Banner />
+  <div class="main-container">
+    <h1>Monte o seu burger:</h1>
+    <Burguerform />
+  </div>
+</template>
+
+<script>
+import Banner from "../components/Banner.vue"
+import Burguerform from "../components/Burguerform.vue"
+
+export default {
+  name:"Home",
+  components: {
+    Banner,
+    Burguerform
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
